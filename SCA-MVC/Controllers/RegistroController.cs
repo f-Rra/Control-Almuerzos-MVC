e09f3ff48f4c481f69d1e05036907bfd3eecc5f3
f-Rra <facundo.rra@gmail.com>
2026-02-09 23:18:52 -0300

    feat: diseño de vista de registro manual - Filtros por empresa y búsqueda por nombre - Tabla de empleados sin almorzar con selección múltiple - Resumen de registrados vs pendientes - Botón de registro masivo

---
+++ SCA-MVC/Controllers/RegistroController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SCA_MVC.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
